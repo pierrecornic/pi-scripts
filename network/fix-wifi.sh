e5e2e6076e1b142e6f0ea546e11c0ca818901103
Gildas Cornic <gildas@cornic.net>
2017-08-20 09:47:44 +0000

    Adding script that restarts wifi when connection is down

---
+++ network/fix-wifi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SERVERIP=192.168.1.1
+ping -c 3 $SERVERIP > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	echo `date` "Wifi down"
+	/sbin/ifconfig wlan0 down >> /var/log/wifi.log 2>&1
+	sleep 2
+	/sbin/ifconfig wlan0 up >> /var/log/wifi.log 2>&1
+	echo `date` "Wifi up"
+fi
